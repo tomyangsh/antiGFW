e937d8e49ed49efb9d0cbddfd3415f4d5f4a25a7
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2023-10-29 12:56:28 -0400

    fetch domains from release branch

--- genpac.py
+++ genpac.py
@@ -1,19 +1,17 @@
 import re
 
-def parse_domian(file):
-    element_list = open('domain-list-community/data/' + file).read().split()
+def genpac(port, name='', file='geolocation-!cn', custom_list=[]):
+    domain_list = custom_list
+    element_list = open(f"domain-list-community/{file}.txt").read().split()
+
     for i in element_list:
-            if re.match('[\w.-]+\.\w+$', i, re.A):
-                    domain_list.append(i)
-            elif re.match('include:(\S+)', i):
-                    parse_domian(re.match('include:(\S+)', i).group(1))
+        parse = re.match('(domain|full):([\w.-]+\.\w+)$', i, re.A)
+        if parse:
+            domain_list.append(parse.group(2))
 
-domain_list = []
-parse_domian('geolocation-!cn')
-domain_list.sort()
-domain_string = "',\n\t'".join(domain_list)
+    domain_list.sort()
+    domain_string = "',\n\t'".join(domain_list)
 
-def genpac(port):
     pac = f'''var proxy = 'SOCKS5 127.0.0.1:{port}; DIRECT';
 var rules = [
 \t'{domain_string}'
@@ -25,7 +23,8 @@ var rules = [
         return proxy;
     }
 }'''
-    open(str(port) + '.pac', 'w').write(pac)
+    open(str(name or port) + '.pac', 'w').write(pac)
 
 genpac(7890)
 genpac(9001)
+genpac(9001, 'cn', 'cn', ['i.duan.red'])
