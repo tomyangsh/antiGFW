92adae7f3716a7137d70714940209ef80d50fc1c
Tom Yang <20330249+tomyangsh@users.noreply.github.com>
2023-10-29 23:34:20 -0400

    genpac: update cn_list

--- genpac.py
+++ genpac.py
@@ -1,6 +1,6 @@
 import re
 
-def genpac(port, name='', file='geolocation-!cn', custom_list=[]):
+def genpac(port, name='', file='geolocation-!cn', custom_list=[], host='127.0.0.1'):
     domain_list = custom_list[:]
     element_list = open(f"domain-list-community/{file}.txt").read().split()
 
@@ -12,7 +12,7 @@ def genpac(port, name='', file='geolocation-!cn', custom_list=[]):
     domain_list.sort()
     domain_string = "',\n\t'".join(domain_list)
 
-    pac = f'''var proxy = 'SOCKS5 127.0.0.1:{port}; DIRECT';
+    pac = f'''var proxy = 'SOCKS5 {host}:{port}; DIRECT';
 var rules = [
 \t'{domain_string}'
 ]
@@ -27,4 +27,10 @@ var rules = [
 
 genpac(7890)
 genpac(9001)
-genpac(9001, 'cn', 'cn', ['i.duan.red'])
+
+cn_list = [
+        'i.duan.red',
+        'l.qq.com'
+        ]
+
+genpac(9001, name='cn', file='cn', custom_list=cn_list)
